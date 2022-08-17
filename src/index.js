d9170d4ee89c61c8f737db2d80a3245e83ba59d9
Danyil-Repenko <danyil.oficial@gmail.com>
2022-08-17 12:48:46 +0300

    дістаємо дані з API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries.js';
 import debounce from "lodash.debounce";
 import { Notify } from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+    input: document.querySelector('input#search-box'),
+    countryList: document.querySelector('ul.country-list'),
+    countryInfo: document.querySelector('div.country-info'),
+}
+
+refs.input.addEventListener("input", debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput(event) {
+    fetchCountries(event.target.value)
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+};
+
+
