240cdb6060aeb24b81362e7ca51242d0baa709b0
Danyil-Repenko <danyil.oficial@gmail.com>
2022-08-19 16:18:43 +0300

    Структуризація

--- src/index.js
+++ src/index.js
@@ -23,55 +23,73 @@ function onInput(event) {
     }
 
     fetchCountries(inputValue)
-        .then(data => {
-            showCountreis(data);
-        })
-        .catch(() => {
-            refs.countryList.innerHTML = '';
-            refs.countryInfo.innerHTML = '';
-            Notify.failure("Oops, there is no country with that name");
-        });
+        .then(manageCountries)
+        .catch(operateError);
 }
 
-function showCountreis(data) {
+function operateError() {
+    refs.countryList.innerHTML = '';
+    refs.countryInfo.innerHTML = '';
+    Notify.failure("Oops, there is no country with that name");
+}
+
+function manageCountries(data) {
     if (data.length === 1) {
-        refs.countryList.innerHTML = '';
-        const markup = data
-            .map((country) => {
-                return `
+        loadMatch(data);
+    } else if (data.length >= 2 && data.length <= 10) {
+        loadTheMatches(data);
+    } else {
+        tooManyMatchesFound();
+    }
+}
+
+function loadMatch(data) {
+    refs.countryList.innerHTML = '';
+    refs.countryInfo.innerHTML = matchMarkup(data);
+}
+
+function matchMarkup(data) {
+    return data
+        .map(({ flags, name, capital, population, languages }) => {
+            return `
           <div class="country-info_head">
             <div class="country-info_flag">
-            <img class="image-item" src="${country.flags.svg}" alt="flag of ${country.name.official}">
+            <img class="image-item" src="${flags.svg}" alt="flag of ${name.official}">
             </div>
-            <p class="country-info_name">${country.name.official}</p>
+            <p class="country-info_name">${name.official}</p>
           </div>
           <div class="country-info_details">
-          <p class="info-text"><b>Capital:</b> ${country.capital}</p>
-          <p class="info-text"><b>Population:</b> ${country.population}</p>
-          <p class="info-text"><b>Languages:</b> ${Object.values(country.languages).join(", ")}</p >
+          <p class="info-text"><b>Capital:</b> ${capital}</p>
+          <p class="info-text"><b>Population:</b> ${population}</p>
+          <p class="info-text"><b>Languages:</b> ${Object.values(languages).join(", ")}</p >
           </div >
         `;
-            })
-            .join("");
-        refs.countryInfo.innerHTML = markup;
-    } else if (data.length >= 2 && data.length <= 10) {
-        refs.countryInfo.innerHTML = '';
-        const markup = data
-            .map((country) => {
-                return `
+        })
+        .join("");
+}
+
+function loadTheMatches(data) {
+    refs.countryInfo.innerHTML = '';
+    refs.countryList.innerHTML = theMatchesMarkup(data);
+}
+
+function theMatchesMarkup(data) {
+    return data
+        .map(({ flags, name }) => {
+            return `
         <li class="country-list_item">
             <div class="country-list_flag">
-            <img class="image-item" src="${country.flags.svg}" alt="flag of ${country.name.official}">
+            <img class="image-item" src="${flags.svg}" alt="flag of ${name.official}">
             </div>
-            <p>${country.name.official}</p>
+            <p>${name.official}</p>
           </li>
         `;
-            })
-            .join("");
-        refs.countryList.innerHTML = markup;
-    } else {
-        refs.countryList.innerHTML = '';
-        refs.countryInfo.innerHTML = '';
-        Notify.info('Too many matches found. Please enter a more specific name.')
-    }
+        })
+        .join("");
+}
+
+function tooManyMatchesFound() {
+    refs.countryList.innerHTML = '';
+    refs.countryInfo.innerHTML = '';
+    Notify.info('Too many matches found. Please enter a more specific name.')
 }
