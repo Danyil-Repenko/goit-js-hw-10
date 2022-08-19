b18c1d3ba8069816d2ceef72568d86d97ed728e1
Danyil-Repenko <danyil.oficial@gmail.com>
2022-08-19 13:48:33 +0300

    Увесь функціонал є

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,6 @@
 export function fetchCountries(name) {
-    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+    const rootUrl = "https://restcountries.com/v3.1/name/";
+    return fetch(`${rootUrl}${name}?fields=name,capital,population,flags,languages`)
         .then(response => {
             if (!response.ok) {
                 throw new Error(response.status);
--- src/index.js
+++ src/index.js
@@ -17,7 +17,6 @@ function onInput(event) {
     const inputValue = event.target.value.trim();
 
     if (!inputValue) {
-        console.log('порожньо');
         refs.countryList.innerHTML = '';
         refs.countryInfo.innerHTML = '';
         return;
@@ -27,7 +26,7 @@ function onInput(event) {
         .then(data => {
             showCountreis(data);
         })
-        .catch(error => {
+        .catch(() => {
             refs.countryList.innerHTML = '';
             refs.countryInfo.innerHTML = '';
             Notify.failure("Oops, there is no country with that name");
@@ -36,23 +35,43 @@ function onInput(event) {
 
 function showCountreis(data) {
     if (data.length === 1) {
-        console.log(data);
         refs.countryList.innerHTML = '';
-    } else if (data.length >= 2 && data.length <= 10) {
         const markup = data
             .map((country) => {
                 return `
-          <li class="country-list_item">
+          <div class="country-info_head">
+            <div class="country-info_flag">
+            <img class="image-item" src="${country.flags.svg}" alt="flag of ${country.name.official}">
+            </div>
+            <p class="country-info_name">${country.name.official}</p>
+          </div>
+          <div class="country-info_details">
+          <p class="info-text"><b>Capital:</b> ${country.capital}</p>
+          <p class="info-text"><b>Population:</b> ${country.population}</p>
+          <p class="info-text"><b>Languages:</b> ${Object.values(country.languages).join(", ")}</p >
+          </div >
+        `;
+            })
+            .join("");
+        refs.countryInfo.innerHTML = markup;
+    } else if (data.length >= 2 && data.length <= 10) {
+        refs.countryInfo.innerHTML = '';
+        const markup = data
+            .map((country) => {
+                return `
+        <li class="country-list_item">
             <div class="country-list_flag">
             <img class="image-item" src="${country.flags.svg}" alt="flag of ${country.name.official}">
             </div>
-            <p class="country-list_name">${country.name.official}</p>
+            <p>${country.name.official}</p>
           </li>
-      `;
+        `;
             })
             .join("");
         refs.countryList.innerHTML = markup;
     } else {
+        refs.countryList.innerHTML = '';
+        refs.countryInfo.innerHTML = '';
         Notify.info('Too many matches found. Please enter a more specific name.')
     }
 }
